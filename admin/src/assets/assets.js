b5480d0bc17852552f88fb7fb7a67de3eca7e5eb
umar-khan-csgrad <umar.khan.csgrad@gmail.com>
2024-09-25 08:24:16 +0530

    ..

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-del-backend-s23m.onrender.com'
+export const url = 'https://food-del-backend-g8bx.onrender.com'
 export const currency = '₹'
 
 export const assets ={
